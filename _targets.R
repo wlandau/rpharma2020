e99c9fe298787a36d8b3fd4c56852563c1d16ea3
wlandau-lilly <will.landau@lilly.com>
2020-06-19 12:08:42 -0400

    Change scale

--- _targets.R
+++ _targets.R
@@ -3,7 +3,7 @@ source("R/functions.R")
 options(clustermq.scheduler = "sge", clustermq.template = "sge.tmpl")
 tar_options(packages = c("coda", "spBayesSurv", "tidyverse", "truncnorm"))
 tar_pipeline(
-    tar_target(sim, seq_len(1000), deployment = "local"),
+  tar_target(sim, seq_len(100), deployment = "local"),
   tar_target(mean_treatment, c(10, 15, 20), deployment = "local"),
   tar_target(
     patients,
